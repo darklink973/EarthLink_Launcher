6fe94e54212d22e4a4010a7a514fd686be9e2cae
darkvox <125704185+darklink973@users.noreply.github.com>
2025-01-22 18:23:59 -0300

    Update uninstall.bat

--- installer/uninstall.bat
+++ installer/uninstall.bat
@@ -1,13 +1,14 @@
 @echo off
 
 echo désinstallation en cours...
-rmdir /S /Q "C:\Users\%USERNAME%\AppData\Local\EarthLink_Launcher"
 rmdir /S /Q "C:\Users\%USERNAME%\AppData\Local\EarthLink_Launcher_Updater"
-
 echo désinstallation des racourcis...
-del "C:\%USERNAME%\cyl13\Desktop\Earthlink_Launcher.lnk"
-del "C:\%USERNAME%\cyl13\Desktop\Earthlink_Launcher_Updater.lnk"
+del "C:\Users\%USERNAME%\Desktop\Earthlink_Launcher.lnk"
+del "C:\Users\%USERNAME%\Desktop\Earthlink_Launcher_Updater.lnk"
 rmdir /S /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\EarthLink"
+rmdir /S /Q C:\"Users"\%USERNAME%\"AppData"\"Roaming"\"Earthlink Launcher"
+rmdir /S /Q "C:\Users\%USERNAME%\AppData\Roaming\.earthlinklauncher"
 
 echo fini !
+rmdir /S /Q "C:\Users\%USERNAME%\AppData\Local\EarthLink_Launcher"
 PAUSE
